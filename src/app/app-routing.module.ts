c4fea1f01da42a2a34de9fb13e31df58c1687519
AlenS <alenskupnjak@yahoo.com>
2020-09-15 12:35:28 +0200

    002 routing implement

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterModule, Routes } from "@angular/router";
+
+import { DashboardComponent } from "./components/dashboard/dashboard.component";
+import { AddClientComponent } from "./components/add-client/add-client.component";
+import { ClientDetailComponent } from "./components/client-detail/client-detail.component";
+import { ClientsComponent } from "./components/clients/clients.component";
+import { EditClientComponent } from "./components/edit-client/edit-client.component";
+import { LoginComponent } from "./components/login/login.component";
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { NotFoundComponent } from "./components/not-found/not-found.component";
+import { RegisterComponent } from "./components/register/register.component";
+import { SettingsComponent } from "./components/settings/settings.component";
+import { SidebarComponent } from "./components/sidebar/sidebar.component";
+
+const routes: Routes = [
+  { path: "", component: DashboardComponent },
+  { path: "login", component: LoginComponent },
+  { path: "register", component: RegisterComponent },
+  { path: "client/add", component: AddClientComponent },
+  { path: "client/edit/:id", component: EditClientComponent },
+  { path: "client/:id", component: ClientDetailComponent },
+  { path: "settings", component: SettingsComponent },
+  { path: "**", component: NotFoundComponent },
+];
+
+@NgModule({
+  exports: [RouterModule],
+  declarations: [],
+  imports: [RouterModule.forRoot(routes)],
+})
+export class AppRoutingModule {}
